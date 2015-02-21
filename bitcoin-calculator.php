e069d9dc9031c84d28724da9c95a8808b4852863
CakeBet <CakeBet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-21 02:20:28 +0000

    tagging version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/bitcoin-currency-converter/trunk@1095652 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bitcoin-calculator.php
+++ bitcoin-calculator.php
@@ -1,28 +1,55 @@
-<?php
-	/*
-	Plugin Name: Bitcoin Calculator Widget
-	Plugin URI: http://www.richardmacarthy.com/
-	Description: A plugin which creates a sidebar widget to calculate your chosen currency to Bitcoin.
-	Author: Richard Macarthy
-	Version: 1.0
-	Author URI: http://www.richardmacarthy.com/
-	*/
-	
-	/************************************************
-	* Global Variables
-	/************************************************/
-	
-	global $wp_roles;
-	
-	register_activation_hook(__FILE__, 'plugin_install');
-	register_deactivation_hook( __FILE__, 'plugin_unininstall' );
-	
-	/************************************************
-	* Includes
-	/************************************************/
-	
-	include('lib/install.php'); 
-	include('lib/admin-page.php'); 
-	include('lib/calculator-widget.php'); 
-	
+<?php
+
+	/*
+
+	Plugin Name: Bitcoin Calculator Widget
+
+	Plugin URI: http://www.richardmacarthy.com/
+
+	Description: A plugin which creates a sidebar widget to calculate your chosen currency to Bitcoin.
+
+	Author: Richard Macarthy
+
+	Version: 1.1
+
+	Author URI: http://www.richardmacarthy.com/
+
+	*/
+
+	
+
+	/************************************************
+
+	* Global Variables
+
+	/************************************************/
+
+	
+
+	global $wp_roles;
+
+	
+
+	register_activation_hook(__FILE__, 'plugin_install');
+
+	register_deactivation_hook( __FILE__, 'plugin_unininstall' );
+
+	
+
+	/************************************************
+
+	* Includes
+
+	/************************************************/
+
+	
+
+	include('lib/install.php'); 
+
+	include('lib/admin-page.php'); 
+
+	include('lib/calculator-widget.php'); 
+
+	
+
 ?>
